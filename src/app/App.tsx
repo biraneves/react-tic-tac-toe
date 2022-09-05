0806e1a1d0bfffba9aa546356a98c027684586bc
biraneves <ubirajara.neves@usp.br>
2022-09-04 22:27:53 -0300

    Commit inicial.

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import { Provider } from 'react-redux';
+import { TicTacToe } from './TicTacToe';
+
+function App() {
+    return <TicTacToe />;
+}
+
+export default App;
---
+++ src/app/TicTacToe.tsx
@@ -0,0 +1,25 @@
+export function TicTacToe() {
+    return (
+        <div className="ticTacToe">
+            <table>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
