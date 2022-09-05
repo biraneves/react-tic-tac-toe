5332cce6e842097de9641891e9e36fa18615c033
biraneves <ubirajara.neves@usp.br>
2022-09-05 15:38:50 -0300

    Status inicial

--- src/app/TicTacToe.tsx
+++ src/app/TicTacToe.tsx
@@ -1,25 +1,50 @@
+import PayloadAction from 'react-redux';
+
+type CellValue = 'X' | 'O' | '';
+
+interface ITicTacToeState {
+    nextPlayer: 'X' | 'O';
+    board: CellValue[][];
+}
+
+const initialState: ITicTacToeState = {
+    nextPlayer: 'X',
+    board: [
+        ['', '', ''],
+        ['', '', 'X'],
+        ['', 'O', ''],
+    ],
+};
+
+type ActionPlay = PayloadAction<{ i: number; j: number }>;
+
+function TicTacToeReducer(state = initialState, action): ITicTacToeState {}
+
 export function TicTacToe() {
+    const state: ITicTacToeState = {
+        nextPlayer: 'X',
+        board: [
+            ['', '', ''],
+            ['', '', 'X'],
+            ['', 'O', ''],
+        ],
+    };
+
     return (
         <div className="ticTacToe">
+            <div>Aguardando jogada de {state.nextPlayer}</div>
             <table>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {state.board.map((row, i) => (
+                        <tr key={i}>
+                            {row.map((cell, j) => (
+                                <td key={j}>{cell}</td>
+                            ))}
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+            <button>Reiniciar partida</button>
         </div>
     );
 }
